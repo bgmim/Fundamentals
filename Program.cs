09ecaf28ad4ef98148ce22e4347a7ab0a0b30629
bgmim <35568484+bgmim@users.noreply.github.com>
2018-01-25 10:38:53 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _12._Number_checker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+
+            while (true)
+            {
+                try
+                {
+                    number = int.Parse(Console.ReadLine());
+                    break;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Invalid input!");
+                   
+                }
+
+            }
+            Console.WriteLine("It is a number.");
+
+        }
+    }
+}
